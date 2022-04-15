c0b93f0e29defcda87a101d6c75f03a52fce6b92
bnpysse <bnpysse@aliyun.com>
2022-04-15 21:33:27 +0800

    Python_Study_Guide_Fourth Edition 2022年4月8日

---
+++ Python_Study_Guide/firstClass.py
@@ -0,0 +1,40 @@
+class FirstClass:
+
+    def setdata(self, value):
+        self.data = value
+
+    def display(self):
+        print(self.data)
+
+
+class SecondClass(FirstClass):
+    def display(self):
+        print('Current vaule = "%s"' % self.data)
+
+
+class ThirdClass(SecondClass):
+    def __init__(self, value):
+        self.data = value
+
+    def __add__(self, other):
+        return ThirdClass(self.data + other)
+
+    def __str__(self):
+        return '[ThirdClass: %s]' % self.data
+
+    def mul(self, other):
+        self.data *= other
+
+
+
+if __name__ == '__main__':
+    a = ThirdClass('abc')
+    a.display()
+    print(a)
+
+    b = a + 'xyz'
+    b.display()
+    print(b)
+
+    a.mul(3)
+    print(a)
